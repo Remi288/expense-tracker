12d650ded888749b4e3e8eadc1b6040929baff90
remi288 <remizcode200@yahoo.com>
2022-07-12 14:39:25 +0100

    Update graph data

--- client/src/components/Graph.js
+++ client/src/components/Graph.js
@@ -1,51 +1,44 @@
-import React from 'react'
-import { Doughnut } from 'react-chartjs-2'
-import {Chart, ArcElement} from 'chart.js'
-import Label from './Label';
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
+import { Chart, ArcElement } from "chart.js";
+import Label from "./Label";
+import { chartData, getTotal } from "../utility/helper";
+import { useGetLabelsQuery } from "../store/expenseApi";
 
-Chart.register(ArcElement)
-
-
-
-const config = {
-  data: {
-    datasets: [
-      {
-        data: [300, 50, 100],
-        backgroundColor: [
-          "rgb(255, 99, 132)",
-          "rgb(54, 162, 235)",
-          "rgb(255, 205, 86)",
-        ],
-        hoverOffset: 4,
-        borderRadius: 30,
-        spacing: 10,
-      },
-    ],
-  },
-  options: {
-    cutout: 120,
-  },
-};
+Chart.register(ArcElement);
 
 function Graph() {
+  const { data, isError, isFetching, isSuccess } = useGetLabelsQuery();
+  let graphData;
+
+  if (isFetching) {
+    graphData = <div>Loading...</div>;
+  } else if (isSuccess) {
+    graphData = <Doughnut {...chartData(data)} />;
+  } else if (isError) {
+    graphData = <div>Error</div>;
+  }
+
   return (
     <div className="flex justify-content max-w-xs mx-auto">
       <div className="item">
         <div className="chart relative">
           {/* charts */}
-          <Doughnut {...config}></Doughnut>
+          {graphData}
           <h3 className="mb-4 font-bold title">
-            Total<span className='block text-3xl text-emerald-400'>&#8358;{0}</span>
+            Total
+            <span className="block text-3xl text-emerald-400">
+              &#8358;{getTotal(data)}
+            </span>
           </h3>
         </div>
         <div className="flex flex-col py-10 gap-4">
-        {/* labels */}
-        <Label />
+          {/* labels */}
+          <Label />
         </div>
       </div>
     </div>
   );
 }
 
-export default Graph
+export default Graph;
